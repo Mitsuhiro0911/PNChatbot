557f86aeee0274bb5f380b238c96c7948ac84ad7
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-06-23 13:43:22 +0900

    形態素解析の処理をParser.ktのparseメソッドに切り出した。

--- src/Main.kt
+++ src/Main.kt
@@ -9,31 +9,7 @@ fun main(args: Array<String>) {
         "sh", "-c",
         "echo ${message} | mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
     )
-    // コマンド結果をProcessで受け取る
-    val ps = Runtime.getRuntime().exec(command)
-    // 標準出力
-    val bReader_i = BufferedReader(InputStreamReader(ps.inputStream, "UTF-8"))
-    // 標準出力を1行ずつ受け取る一時オブジェクト
-    var targetLine: String?
-    // 形態素解析結果を全て解析する
-    while (true) {
-        // 形態素解析結果を1行ずつ受け取る
-        targetLine = bReader_i.readLine()
-        // 最終行まで解析が完了したらループを抜ける
-        if (targetLine == null) {
-            break
-        } else if (targetLine == "EOS") {
-            continue
-        } else {
-            // 品詞
-            val targetType = targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-            val word = targetLine.split("\t".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-            if (targetType == "名詞") {
-                println(word)
-            }
-        }
-    }
-    // 終了を待つ
-    ps.waitFor()
+
+    Parser().parse(command)
 
 }
