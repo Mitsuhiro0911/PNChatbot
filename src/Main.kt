460c3c11f915a0176ae6a932b767fa244e1834bf
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-06-30 21:26:14 +0900

    発言を不要に毎回形態素解析していたため修正。 Parse.ktでmakeWordListメソッドで発言と返答を纏めて形態素解析していたが、それぞれメソッドを分割した。

--- src/Main.kt
+++ src/Main.kt
@@ -8,35 +8,41 @@ fun main(args: Array<String>) {
     val message = "この写真はインスタ映えしますね。"
 //    val reverseMessage = "私の趣味は写真を取ることです。写真"
     val cal = Calculator()
-
-    val br = BufferedReader(FileReader(File("./data/corpas/nucc/data001.txt")))
-    var reverseMessage = br.readLine()
-    while(reverseMessage != null) {
+    val messageCommand = arrayOf(
+        "sh", "-c",
+        "echo ${message} | mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
+    )
+    val messageWordList = Parser().parseMessage(messageCommand)
+    println(messageWordList)
+    for(i in 1 until 130) {
+        println("data${String.format("%03d", i)}")
+        val br = BufferedReader(FileReader(File("./data/corpas/nucc/data${String.format("%03d", i)}.txt")))
+        var reverseMessage = br.readLine()
+        while (reverseMessage != null) {
 //        println(reverseMessage)
-        val messageCommand = arrayOf(
-            "sh", "-c",
-            "echo ${message} | mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
-        )
-        val reverseCommand = arrayOf(
-            "sh", "-c",
-            "echo ${reverseMessage} | mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
-        )
-        val wordList = Parser().makeWordList(messageCommand, reverseCommand, reverseMessage)
-        if(wordList === null) {
+            val reverseCommand = arrayOf(
+                "sh", "-c",
+                "echo ${reverseMessage} | mecab -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd"
+            )
+            val wordList = Parser().parseReverseMessage(reverseCommand, messageWordList, reverseMessage)
+            // 発言と返答の名詞が全く重複しない場合、その後の処理は冗長なのでスキップする
+            if (wordList === null) {
+                reverseMessage = br.readLine()
+                continue
+            }
+            println(reverseMessage)
+            val messaseTfMap = Parser().calTf(messageCommand, wordList)
+            println(messaseTfMap)
+
+
+            val reverseMessaseTfMap = Parser().calTf(reverseCommand, wordList)
+            println(reverseMessaseTfMap)
+
+            val messageVector = messaseTfMap.values.toDoubleArray()
+            val reverseMessageVector = reverseMessaseTfMap.values.toDoubleArray()
+            println(cal.calCosSimilarity(messageVector, reverseMessageVector))
             reverseMessage = br.readLine()
-            continue
+            println("返答：${reverseMessage}")
         }
-
-        val messaseTfMap = Parser().calTf(messageCommand, wordList)
-        println(messaseTfMap)
-
-
-        val reverseMessaseTfMap = Parser().calTf(reverseCommand, wordList)
-        println(reverseMessaseTfMap)
-
-        val messageVector = messaseTfMap.values.toDoubleArray()
-        val reverseMessageVector = reverseMessaseTfMap.values.toDoubleArray()
-        println(cal.calCosSimilarity(messageVector, reverseMessageVector))
-        reverseMessage = br.readLine()
     }
 }
--- src/Parser.kt
+++ src/Parser.kt
@@ -1,60 +1,83 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-class Parser{
-    fun makeWordList(messageCommand: Array<String>, reverseCommand: Array<String>, reverseMessage: String): ArrayList<String>?{
-        val wordList = ArrayList<String>()
-        val command = mutableListOf<Array<String>>()
-        command.add(messageCommand)
-        command.add(reverseCommand)
-        var skipFlag = false
-        // 発言と返答を形態素解析
-        for(com in command) {
-            // 返答を形態素解析する時、発言の名詞が一つも含まれていない場合処理をスキップする(処理高速化のため)
-            if(wordList.size != 0){
-                for(word in wordList) {
-                    if(reverseMessage.contains(word)) {
-                        skipFlag = true
-                    }
-                }
-                if(skipFlag === false) {
-                    return null
-                }
-            }
-            // コマンド結果をProcessで受け取る
-            val ps = Runtime.getRuntime().exec(com)
-            // 標準出力
-            val bReader_i = BufferedReader(InputStreamReader(ps.inputStream, "UTF-8"))
-            // 標準出力を1行ずつ受け取る一時オブジェクト
-            var targetLine: String?
-            // 形態素解析結果を全て解析する
-            while (true) {
-                // 形態素解析結果を1行ずつ受け取る
-                targetLine = bReader_i.readLine()
-                // 最終行まで解析が完了したらループを抜ける
-                if (targetLine == null) {
-                    break
-                } else if (targetLine == "EOS") {
-                    continue
-                } else {
-                    // 品詞
-                    val targetType =
-                        targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-                    val word = targetLine.split("\t".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-                    if (targetType == "名詞") {
-                        wordList.add(word)
-                    }
+class Parser {
+    fun parseMessage(command: Array<String>): ArrayList<String>? {
+        var wordList: ArrayList<String>? = arrayListOf()
+        // コマンド結果をProcessで受け取る
+        val ps = Runtime.getRuntime().exec(command)
+        // 標準出力
+        val bReader_i = BufferedReader(InputStreamReader(ps.inputStream, "UTF-8"))
+        // 標準出力を1行ずつ受け取る一時オブジェクト
+        var targetLine: String?
+        // 形態素解析結果を全て解析する
+        while (true) {
+            // 形態素解析結果を1行ずつ受け取る
+            targetLine = bReader_i.readLine()
+            // 最終行まで解析が完了したらループを抜ける
+            if (targetLine == null) {
+                break
+            } else if (targetLine == "EOS") {
+                continue
+            } else {
+                // 品詞
+                val targetType =
+                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
+                val word = targetLine.split("\t".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
+                if (targetType == "名詞") {
+                    wordList?.add(word)
                 }
             }
-            // 終了を待つ
-            ps.waitFor()
         }
+        // 終了を待つ
+        ps.waitFor()
         return wordList
     }
 
-    fun calTf(command: Array<String>, wordList: ArrayList<String>): LinkedHashMap<String, Double>{
+    fun parseReverseMessage(command: Array<String>, messageWordList: ArrayList<String>?, reverseMessage: String): ArrayList<String>? {
+        var wordList = messageWordList?.clone() as ArrayList<String>?
+        var skipFlag = true
+        for (word in wordList!!) {
+            if (reverseMessage.contains(word)) {
+                skipFlag = false
+            }
+        }
+        if (skipFlag) {
+            return null
+        }
+        // コマンド結果をProcessで受け取る
+        val ps = Runtime.getRuntime().exec(command)
+        // 標準出力
+        val bReader_i = BufferedReader(InputStreamReader(ps.inputStream, "UTF-8"))
+        // 標準出力を1行ずつ受け取る一時オブジェクト
+        var targetLine: String?
+        // 形態素解析結果を全て解析する
+        while (true) {
+            // 形態素解析結果を1行ずつ受け取る
+            targetLine = bReader_i.readLine()
+            // 最終行まで解析が完了したらループを抜ける
+            if (targetLine == null) {
+                break
+            } else if (targetLine == "EOS") {
+                continue
+            } else {
+                // 品詞
+                val targetType =
+                    targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
+                val word = targetLine.split("\t".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
+                if (targetType == "名詞") {
+                    wordList?.add(word)
+                }
+            }
+        }
+        // 終了を待つ
+        ps.waitFor()
+        return wordList
+    }
+
+    fun calTf(command: Array<String>, wordList: ArrayList<String>): LinkedHashMap<String, Double> {
         val tfMap = LinkedHashMap<String, Double>()
-        for(word in wordList){
+        for (word in wordList) {
             tfMap.put(word, 0.0)
         }
         // コマンド結果をProcessで受け取る
