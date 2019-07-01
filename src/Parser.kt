a902ff4db44dcacb7f1b7bd5d88bf0fb2a325823
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-07-01 22:22:38 +0900

    calTfの形態素解析の処理をparseメソッドを用いてリファクタリング

--- src/Parser.kt
+++ src/Parser.kt
@@ -26,33 +26,12 @@ class Parser {
         for (word in wordList) {
             tfMap.put(word, 0.0)
         }
-        // コマンド結果をProcessで受け取る
-        val ps = Runtime.getRuntime().exec(command)
-        // 標準出力
-        val bReader_i = BufferedReader(InputStreamReader(ps.inputStream, "UTF-8"))
-        // 標準出力を1行ずつ受け取る一時オブジェクト
-        var targetLine: String?
-        // 形態素解析結果を全て解析する
-        while (true) {
-            // 形態素解析結果を1行ずつ受け取る
-            targetLine = bReader_i.readLine()
-            // 最終行まで解析が完了したらループを抜ける
-            if (targetLine == null) {
-                break
-            } else if (targetLine == "EOS") {
-                continue
-            } else {
-                // 品詞
-                val targetType = targetLine.split("[\t|,]".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-                val word = targetLine.split("\t".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-                if (targetType == "名詞") {
-                    // 同じ単語が出現する毎に出現回数をカウントアップする
-                    tfMap.put(word, tfMap.get(word)!!.plus(1.0))
-                }
-            }
+        var wordCountList: ArrayList<String>? = arrayListOf<String>()
+        wordCountList = parse(command, wordCountList)
+        for(wordCount in wordCountList!!){
+            // 同じ単語が出現する毎に出現回数をカウントアップする
+            tfMap.put(wordCount, tfMap.get(wordCount)!!.plus(1.0))
         }
-        // 終了を待つ
-        ps.waitFor()
         return tfMap
     }
     fun parse(command: Array<String>, wordList: ArrayList<String>?): ArrayList<String>?{
