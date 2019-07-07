8393403e0593eca3ad1b7dc95b06a526ba384cda
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-07-07 16:01:33 +0900

    IDFの計算に利用するためのdata001.txt〜data130.txtの全会話数をカウントし、corpas_lime_num.txtへ出力する処理を追加。

--- src/AdjustCorpas.kt
+++ src/AdjustCorpas.kt
@@ -5,9 +5,11 @@ import java.io.FileReader
 import java.io.FileWriter
 
 fun main(args: Array<String>) {
+    // IDFの計算に利用するため、data001.txt〜data130.txtの全会話数をカウントする変数
+    var lineCount = 0
     for(i in 1 until 130) {
         val br = BufferedReader(FileReader(File("./data/corpas/nucc/data${String.format("%03d", i)}.txt")))
-        val bw = BufferedWriter(FileWriter(File("./data/corpas/nucc_adjust/data${String.format("%03d", i)}.txt")))
+        val bwAdjustCorpas = BufferedWriter(FileWriter(File("./data/corpas/nucc_adjust/data${String.format("%03d", i)}.txt")))
         val participantList = mutableListOf<String>()
         var str = br.readLine()
         while(str.get(0) == '＠'){
@@ -16,36 +18,42 @@ fun main(args: Array<String>) {
                     participantList.add(str.substring(4, 8))
                 }
             }
-            bw.write(str)
-            bw.newLine()
+            bwAdjustCorpas.write(str)
+            bwAdjustCorpas.newLine()
             str = br.readLine()
             println(str)
         }
 
-        bw.write(str)
+        bwAdjustCorpas.write(str)
+        lineCount = lineCount.plus(1)
         str = br.readLine()
 
         println(participantList)
         while (str != null) {
             println(str)
             if(str == "＠ＥＮＤ"){
-                bw.newLine()
-                bw.write(str)
+                bwAdjustCorpas.newLine()
+                bwAdjustCorpas.write(str)
                 break
             }
             var participantFlag = false
             for(participant in participantList){
                 if(str.contains("${participant}：")){
-                    bw.newLine()
-                    bw.write(str)
+                    bwAdjustCorpas.newLine()
+                    bwAdjustCorpas.write(str)
+                    lineCount = lineCount.plus(1)
                     participantFlag = true
                 }
             }
             if(!participantFlag) {
-                bw.write(str)
+                bwAdjustCorpas.write(str)
             }
             str = br.readLine()
         }
-        bw.close()
+        bwAdjustCorpas.close()
     }
+    println("data001.txt〜data130.txtの全会話数：${lineCount}")
+    val bwCorpasLineNum = BufferedWriter(FileWriter(File("./data/feature/corpas_line_num.txt")))
+    bwCorpasLineNum.write("${lineCount}")
+    bwCorpasLineNum.close()
 }
